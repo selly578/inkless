4727aef205dad78171b480bb238d99b9f4c8c2eb
grandmaster <grandmaster75382@gmail.com>
2024-12-05 07:36:55 +0700

    fix migration error

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,25 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_session import Session
+from sqlalchemy import MetaData
 from dotenv import load_dotenv
+from markdown import markdown
 from os import getenv
 
 load_dotenv()
 
 app = Flask(__name__)
-db = SQLAlchemy()
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
 migrate = Migrate()
 _session = Session()
 
