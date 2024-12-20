59d029ec645186b76e5a3f215019e33e1d42cdb4
grandmaster <grandmaster75382@gmail.com>
2024-12-21 06:52:45 +0700

    remove non api routes

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -6,40 +6,6 @@ from .. import db,limiter
 
 user = Blueprint("user",__name__)
 
-@user.get("/")
-def profile():    
-    posts = Post.query.filter_by(author=session["identity"]).all()
-    return render_template("profile.html",posts=posts)
-
-@user.post("/")
-def save_profile():
-    nickname = request.form.get("nickname")    
-    user = User.query.filter_by(code=session["identity"]).first()
-
-    if user:
-        user.nickname = nickname 
-    session["nickname"] = nickname
-    db.session.commit()
-
-    return redirect(url_for("user.profile"))
-
-@user.get("/loadaccess")
-def load_access():
-    return render_template("load.html")
-
-@user.post("/loadaccess")
-def load_access_process():
-    code = request.form.get("code")
-    user_exist = User.query.filter_by(code=code).first()
-    post_exist = Post.query.filter_by(author=code).first()
-
-    if user_exist or post_exist:
-        session["identity"] = code
-        session["nickname"] = user_exist.nickname
-        return redirect(url_for("post.index"))
-
-    return render_template("load.html",error="Identity not exist or not saved")
-
 @user.get("/profile/create")
 @limiter.limit("1 per month")
 def _profile():
